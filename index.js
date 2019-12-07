5a59ab120a569ac767ca908b47330385439962c2
Jason Millen <jasonmillen@jasons-air.home>
2019-12-07 18:09:14 -0500

    Init

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+import { getTokens } from './util/spotifyApi';
+import { runInNewContext } from 'vm';
+
+const app = express();
+app.use(express.json());
+
+const spotifyCredentials = {
+  clientId: '4424b4c861ab4ebbba8c5a432253c2eb',
+  clientSecret: '3c60035d220540bf8b51e1107fe1556a',
+  redirectUri: 'https://auth.expo.io/@jasonmillen/AwesomeProject'
+};
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+})
+
+app.get('/healthcheck', (req, res) => {
+  res.send('Hello World');
+})
+
+app.post('/api/getSpotifyToken', async (req, res) => {
+  console.log('getting spotify token');
+  console.log(req.body);
+  const authCode = req.body.authCode;
+  const tokenData = await getTokens(authCode, spotifyCredentials);
+  console.log("token data: ", tokenData);
+  res.send(tokenData);
+});
+
+app.listen(3000,()=>
+  console.log(`Server is listening on port 3000`)
+);
---
+++ util/spotifyApi.js
@@ -0,0 +1,44 @@
+import { encode as btoa } from 'base-64';
+import fetch from 'node-fetch';
+
+export const getTokens = async (authorizationCode, credentials) => {
+  let tokenData;
+  try {
+    const credsB64 = btoa(`${credentials.clientId}:${credentials.clientSecret}`);
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${credsB64}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${
+        credentials.redirectUri
+      }`,
+    });
+
+    const responseJson = await response.json();
+
+    // destructure the response and rename the properties to be in camelCase to satisfy my linter ;)
+    const {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+      expires_in: expiresIn
+    } = responseJson;
+
+    const expirationTime = new Date().getTime() + expiresIn * 1000;
+    // set user data accessToken
+    // set user data refreshToken
+    // set user data expirationTime
+    tokenData = {
+      accessToken,
+      refreshToken,
+      expirationTime
+    };
+  }
+  catch (err) {
+    console.error(err);
+  }
+
+  return tokenData;
+};
